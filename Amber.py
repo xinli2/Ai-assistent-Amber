52f321b39ef769aa4673b175c502973f436a34fa
Li Xin (Jack) <77429846+xinli2@users.noreply.github.com>
2021-10-20 15:27:45 -0700

    Amber.py
    First Ai I create!!!

---
+++ Amber.py
@@ -0,0 +1,50 @@
+import requests
+import json
+ 
+userid = str('Eliza')
+apikey = '#########'
+ 
+# Create post function
+def robot(content):
+    # Amber api
+    api = r'#########' 
+    # Create post submitted data
+    data = {
+        "perception": {
+            "inputText": {
+                "text": content
+                         }
+                      },
+        "userInfo": {
+                    "apiKey": apikey,
+                    "userId": userid,
+                    }
+    }
+    # Convert to json format
+    jsondata = json.dumps(data)
+    # Initiate a post request
+    response = requests.post(api, data = jsondata)
+    # Decode the returned json data
+    robot_res = json.loads(response.content)
+    # Extract conversation data
+    print(robot_res["results"][0]['values']['text'])
+ 
+for x in range(100):
+    content = input("talk:")
+    # Enter the content of the conversation 
+    robot(content)
+    if x == 100:
+        break 
+ 
+while True:
+    content = input("talk:")
+    # Enter the content of the conversation 
+    robot(content)
+    if content == 'bye':
+    # Set stopwords
+        break
+ 
+# # Create an endless loop of dialogue
+# while True:
+# # Enter the content of the conversation
+# content = input("talk:")
